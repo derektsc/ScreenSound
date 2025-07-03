b85741eff914fe11bcf4cf214b5f4aa942b33791
Derek Barragão <42359941+derektsc@users.noreply.github.com>
2025-07-03 02:52:00 -0300

    Enviando arquivos

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,24 @@
+﻿class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+    public string DescricaoResumida => $"A música '{Nome}' de {Artista} tem duração de {Duracao} segundos."; // Propriedade somente leitura Muito comum usarem "=>" se chama Lambda Expression
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao} segundos");
+        if (Disponivel)
+        {
+            Console.WriteLine("A música está disponível para reprodução.");
+        }
+        else
+        {
+            Console.WriteLine("A música não está disponível no momento.");
+        }
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,17 @@
+﻿Musica musica1 = new Musica();
+
+musica1.Nome = "Bohemian Rhapsody";
+musica1.Artista = "Queen";
+musica1.Duracao = 354; // duração em segundos  
+musica1.Disponivel = true; // Define a música como disponível
+Console.WriteLine(musica1.DescricaoResumida); // Exibe a descrição resumida da música
+
+Musica musica2 = new Musica();
+
+musica2.Nome = "Imagine";
+musica2.Artista = "John Lennon";
+musica2.Duracao = 183; // duração em segundos
+musica2.Disponivel = false; // Define a música como não disponível
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
